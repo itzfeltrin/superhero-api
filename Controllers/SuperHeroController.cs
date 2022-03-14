184af3547181c6aa42b6e8aa539ab9591d1a7c21
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-03-14 15:10:36 -0300

    create delete hero route

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -16,7 +16,8 @@ public class SuperHeroController : ControllerBase
             LastName = "Wayne",
             Place = "Gotham City"
         },
-        new SuperHero {
+        new SuperHero
+        {
             Id = 2,
             Name = "Iron Man",
             FirstName = "Tony",
@@ -24,7 +25,7 @@ public class SuperHeroController : ControllerBase
             Place = "Long Island"
         }
     };
-    
+
     [HttpGet]
     public async Task<ActionResult<List<SuperHero>>> Get()
     {
@@ -32,16 +33,16 @@ public class SuperHeroController : ControllerBase
     }
 
     [HttpGet("{heroId:int}")]
-    public async Task<ActionResult<SuperHero>> Get([FromRoute]int heroId)
+    public async Task<ActionResult<SuperHero>> Get([FromRoute] int heroId)
     {
         var hero = heroes.Find(hero => hero.Id.Equals(heroId));
         if (hero == null)
             return BadRequest("Hero not found");
         return Ok(hero);
     }
-    
+
     [HttpPost]
-    public async Task<ActionResult<List<SuperHero>>> AddHero([FromBody]SuperHero hero)
+    public async Task<ActionResult<List<SuperHero>>> AddHero([FromBody] SuperHero hero)
     {
         heroes.Add(hero);
         return Ok(heroes);
@@ -51,15 +52,22 @@ public class SuperHeroController : ControllerBase
     public async Task<ActionResult<List<SuperHero>>> UpdateHero(int heroId, [FromBody] SuperHero request)
     {
         var hero = heroes.Find(hero => hero.Id.Equals(heroId));
-
         if (hero == null)
             return BadRequest("Hero not found");
-
         hero.Name = request.Name;
         hero.FirstName = request.FirstName;
         hero.LastName = request.LastName;
         hero.Place = request.Place;
-
         return Ok(heroes);
+    }
+
+    [HttpDelete("{heroId:int}")]
+    public async Task<ActionResult<List<SuperHero>>> DeleteHero(int heroId)
+    {
+        var hero = heroes.Find(hero => hero.Id.Equals(heroId));
+        if (hero == null)
+            return BadRequest("Hero not found");
+        heroes.Remove(hero);
+        return heroes;
     }
 }
