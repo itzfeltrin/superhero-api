e47ffd571b149d63ced815b197e20cd20c885fef
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-03-14 15:05:17 -0300

    create delete hero route

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -46,4 +46,20 @@ public class SuperHeroController : ControllerBase
         heroes.Add(hero);
         return Ok(heroes);
     }
+
+    [HttpPut("{heroId:int}")]
+    public async Task<ActionResult<List<SuperHero>>> UpdateHero(int heroId, [FromBody] SuperHero request)
+    {
+        var hero = heroes.Find(hero => hero.Id.Equals(heroId));
+
+        if (hero == null)
+            return BadRequest("Hero not found");
+
+        hero.Name = request.Name;
+        hero.FirstName = request.FirstName;
+        hero.LastName = request.LastName;
+        hero.Place = request.Place;
+
+        return Ok(heroes);
+    }
 }
